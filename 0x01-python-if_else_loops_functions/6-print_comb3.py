b57923ecbe5cb343029000932eab0526d255f195
chmakha2002 <chmakha2002@gmail.com>
2024-10-04 00:03:24 +0000

    nice

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3\
+#!/usr/bin/python3
 for i in range(10):
-    print("{}".format(", ".join(f"{i}{j}"  for j in range(i + 1, 10))))
+    for j in range(i + 1, 10):
+        print("{}".format(f"{i}{j}"),end=", ")
