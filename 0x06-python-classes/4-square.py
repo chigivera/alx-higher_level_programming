9a1cdf6337fe4efd4b6c007ee2c09758205215ca
chmakha2002 <chmakha2002@gmail.com>
2024-10-13 16:03:18 +0000

    0x06

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,9 +14,11 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -24,5 +26,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size ** 2
